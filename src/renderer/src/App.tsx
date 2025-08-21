b47d8f18158bfdc1c288c4ff4b4cc8507c3e62c8
diveshc <drchudasama26@gmail.com>
2025-08-21 16:57:36 -0400

    environment setup

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App(): React.JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! test
+      </h1>
+    </>
+  )
+}
+
+export default App
