1df36c6fcb6ce5c1cafbb29730de178b3e8dcfc2
diveshc <drchudasama26@gmail.com>
2025-08-20 22:07:24 -0400

    switch to typescript

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",     // root file
+    "./src/**/*.{ts,tsx,js,jsx,html}" // renderer/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
